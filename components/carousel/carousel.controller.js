9bea8290533ebec13187d6ad3f6437304a8fd800
Kevin Warrington <kevin.warrington@gmail.com>
2023-07-14 13:11:49 -0600

    feat: update carousel to use swiper

--- components/carousel/carousel.controller.js
+++ components/carousel/carousel.controller.js
@@ -1,48 +1,29 @@
-import { Carousel } from "flowbite";
 import { Controller } from "@hotwired/stimulus";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default class extends Controller {
-  static targets = ["indicator", "item", "prev", "next"];
+  static targets = ["prev", "next", "pagination"];
   connect() {
-    const items = this.itemTargets.map((el, i) => ({
-      el,
-      position: i,
-    }));
-    const indicators = this.indicatorTargets.map((el, i) => ({
-      el,
-      position: i,
-    }));
-
-    const options = {
-      defaultPosition: 0,
-      interval: 3000,
-
-      indicators: {
-        activeClasses: "bg-blue-700",
-        inactiveClasses: "bg-white",
-        items: indicators,
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      autoHeight: true,
+      modules: [Navigation, Pagination],
+      speed: 500,
+      navigation: {
+        nextEl: this.nextTarget,
+        prevEl: this.prevTarget,
       },
-
-      // callback functions
-      onNext: () => {
-        console.log("next slider item is shown");
+      pagination: {
+        el: this.paginationTarget,
+        bulletClass:
+          "swiper-pagination-bullet !block !w-3 !h-3 !rounded-full !bg-white !border-2 !border-white !opacity-100",
+        bulletActiveClass:
+          "swiper-pagination-bullet-active !bg-transparent !border-transparent",
       },
-      onPrev: () => {
-        console.log("previous slider item is shown");
-      },
-      onChange: () => {
-        console.log("new slider item has been shown");
-      },
-    };
-
-    const carousel = new Carousel(items, options);
-
-    this.prevTarget.addEventListener("click", () => {
-      carousel.prev();
-    });
-
-    this.nextTarget.addEventListener("click", () => {
-      carousel.next();
     });
   }
 }
