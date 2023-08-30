f28d3f9321beddfe0d1f327d41cad1dec1b5708e
Michael Sabares <michael.sabares@zu.com>
2023-08-30 16:33:52 -0600

    fix: storybook url points to rapidkit url

--- src/StarterKit.php
+++ src/StarterKit.php
@@ -17,22 +17,28 @@ final class StarterKit implements StarterKitInterface {
     // Process info.yml file.
     $info_file = "$working_dir/$machine_name.info.yml";
     $info = Yaml::decode(file_get_contents($info_file));
-    unset($info['hidden']);
-    unset($info['starterkit']);
+    unset($info['hidden'], $info['starterkit']);
     file_put_contents($info_file, Yaml::encode($info));
 
     // Process webpack.config.js file
     $webpack_file = "$working_dir/webpack.config.js";
     $webpack = file_get_contents($webpack_file);
-    $webpack = str_replace($starterkit_machine_name, $machine_name, $webpack);
-    $webpack = str_replace('/contrib/', '/custom/', $webpack);
+    $webpack = str_replace(
+        [$starterkit_machine_name, '/contrib/'],
+        [$machine_name, '/custom/'],
+        $webpack
+    );
     file_put_contents($webpack_file, $webpack);
 
     // Process lando.yml file
-    if(array_key_exists('LANDO', $_ENV) && $_ENV['LANDO'] == 'ON') {
+    if(array_key_exists('LANDO', $_ENV) && $_ENV['LANDO'] === 'ON') {
         $lando_file = "/app/.lando.yml";
         $lando = file_get_contents($lando_file);
-        $lando = str_replace($starterkit_machine_name, $machine_name, $lando);
+        $lando = str_replace(
+            [$starterkit_machine_name, '/contrib/'],
+            [$machine_name, '/custom/'],
+            $lando
+        );
         file_put_contents($lando_file, $lando);
     } else {
         echo "Whao! You tried to generate a theme outside of lando! All good!\nJust make sure your custom commands in your .lando.yml file uses the new theme name!\n\n";
@@ -41,8 +47,11 @@ final class StarterKit implements StarterKitInterface {
     // Process .storybook/preview.js file
     $storybook_file = "$working_dir/.storybook/preview.js";
     $storybook = file_get_contents($storybook_file);
-    $storybook = str_replace($starterkit_machine_name, $machine_name, $storybook);
-    $storybook = str_replace($starterkit_theme_name, $theme_name, $storybook);
+    $storybook = str_replace(
+        [$starterkit_machine_name, $starterkit_theme_name],
+        [$machine_name, $theme_name],
+        $storybook
+    );
     file_put_contents($storybook_file, $storybook);
 
     // Process component files
@@ -50,9 +59,11 @@ final class StarterKit implements StarterKitInterface {
     foreach ($component_files as $component_file) {
         if($component_file->isFile()) {
             $file = file_get_contents($component_file->getPathname());
-            $file = str_replace($starterkit_machine_name, $machine_name, $file);
-            $file = str_replace($starterkit_theme_name, $theme_name, $file);
-            $file = str_replace('/contrib/', '/custom/', $file);
+            $file = str_replace(
+                [$starterkit_machine_name, $starterkit_theme_name, '/contrib/'],
+                [$machine_name, $theme_name, '/custom/'],
+                $file
+            );
             file_put_contents($component_file, $file);
         }
     }
